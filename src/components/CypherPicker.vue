e57eef0ea5f9a9d50e43f27a909fea3656f40b0a
Pierre Jimenez <pjime032@fiu.edu>
2018-09-12 23:50:39 -0400

    Added CypherDisplay logic to CypherPicker

--- src/components/CypherPicker.vue
+++ src/components/CypherPicker.vue
@@ -3,43 +3,53 @@
     <h2>Cypher Picker</h2>
     <h2>Set your rolls</h2>
     <div class="inputs">
-      <input class="inputBox" v-model.number="d100">
+      <label for="d100">d100</label>
+      <input class="inputBox" id="d100" v-model.number="d100">
       <p>d100</p>
     </div>
     <div class="inputs">
-      <input class="inputBox" v-model.number="d6">
+      <label for="d6">d6</label>
+      <input class="inputBox" id="d6" v-model.number="d6">
       <p>d6</p>
     </div>
     <FormButton
       button-text="Salvage"
       @buttonClick="handleButtonClick"
     ></FormButton>
+    <CypherDisplay
+      v-bind:cypher="cypher" v-if="showCypher">
+    </CypherDisplay>
   </div>
 </template>
 
 <script>
 import FormButton from './FormButton';
+import CypherDisplay from './CypherDisplay';
 import cyphers, { Cypher } from '../data/Cyphers';
 
 export default {
   name: 'CypherPicker',
-  components: { FormButton },
+  components: { FormButton, CypherDisplay },
   data() {
     return {
       d100: 0,
       d6: 0,
       cypher: null,
+      showCypher: false,
     };
   },
   methods: {
     handleButtonClick() {
-      const cypherPick = cyphers[this.d100 - 1];
-      this.cypher = new Cypher(
-        cypherPick.name,
-        cypherPick.anoetic,
-        this.d6 + cypherPick.levelModifier,
-        cypherPick.formFactor(),
-        cypherPick.effect);
+      if (this.d100 !== 0 || this.d6 !== 0) {
+        const cypherPick = cyphers[this.d100 - 1];
+        this.cypher = new Cypher(
+          cypherPick.name,
+          cypherPick.anoetic,
+          this.d6 + cypherPick.levelModifier,
+          cypherPick.formFactor(),
+          cypherPick.effect);
+        this.showCypher = true;
+      }
     },
   },
 };
--- test/unit/specs/CypherPicker.spec.js
+++ test/unit/specs/CypherPicker.spec.js
@@ -1,6 +1,8 @@
 import { shallowMount } from '@vue/test-utils';
 import CypherPicker from '@/components/CypherPicker';
 import FormButton from '@/components/FormButton';
+import CypherDisplay from '@/components/CypherDisplay';
+import { Cypher } from '../../../src/data/Cyphers';
 
 describe('CypherPicker.vue', () => {
   let Wrapper;
@@ -24,6 +26,10 @@ describe('CypherPicker.vue', () => {
     expect(Wrapper.contains(FormButton)).toBe(true);
   });
 
+  it('should contain a CypherDisplay', () => {
+    expect(Wrapper.vm.$options.components.CypherDisplay).toBeDefined();
+  });
+
   it('calls handleButtonClick when @buttonClick occurs', () => {
     const stub = jest.fn();
     Wrapper.setMethods({ handleButtonClick: stub });
@@ -39,4 +45,19 @@ describe('CypherPicker.vue', () => {
     expect(Wrapper.vm.$data.cypher.levelModifier).toBe(1);
   });
 
+  it('should not handle click if either roll is 0', () => {
+    Wrapper.setData({ d100: 0, d6: 0, cypher: null });
+    Wrapper.vm.handleButtonClick();
+    expect(Wrapper.vm.$data.cypher).toBeFalsy();
+  });
+
+  it('should not render CypherDisplay if showCypher is false', () => {
+    Wrapper.setData({ cypher: null, showCypher: false });
+    expect(Wrapper.contains(CypherDisplay)).toBe(false);
+  });
+
+  it('should render CypherDisplay if showCypher is true', () => {
+    Wrapper.setData({ cypher: new Cypher('something', true, 1, ['fine'], 'okay'), showCypher: true });
+    expect(Wrapper.contains(CypherDisplay)).toBe(true);
+  });
 });
