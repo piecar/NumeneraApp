54c6fd7a759094f87ffcca2ca39c673c01e7fe3a
Pierre Jimenez <pjime032@fiu.edu>
2019-03-09 01:02:29 -0500

    Removed Phonegap

--- README.md
+++ README.md
@@ -1,99 +1,26 @@
-# numeneraApp
+# treetest
 
-> A Vue.js project
-
-## Build Setup
-
-``` bash
-# install dependencies
+## Project setup
+```
 npm install
-
-# serve with hot reload at localhost:8080
-npm run dev
-
-# build for production with minification
-npm run build
-
-# build for production and view the bundle analyzer report
-npm run build --report
-
-# run unit tests
-npm run unit
-
-# run unit tests with watch
-npm run unit -- --watch
-
-# run all tests
-npm test
 ```
 
-For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
 
+### Compiles and minifies for production
+```
+npm run build
+```
 
-# PhoneGap
+### Lints and fixes files
+```
+npm run lint
+```
 
-## Usage
-
-#### PhoneGap CLI
-
-The hello-world template is the default when you create a new application using the [phonegap-cli][phonegap-cli-url].
-
-    phonegap create my-app
-
-Create an app using this template specifically:
-
-    phonegap create my-app --template hello-world
-
-To see a list of other available PhoneGap templates:
-
-    phonegap template list
-
-## [config.xml][config-xml]
-
-#### android-minSdkVersion (Android only)
-
-Minimum SDK version supported on the target device. Maximum version is blank by default.
-
-This template sets the minimum to `14`.
-
-    <preference name="android-minSdkVersion" value="14" />
-
-#### &lt;access ...&gt; (All)
-
-This template defaults to wide open access.
-
-    <access origin="*" />
-
-It is strongly encouraged that you restrict access to external resources in your application before releasing to production.
-
-For more information on whitelist configuration, see the [Cordova Whitelist Guide][cordova-whitelist-guide] and the [Cordova Whitelist Plugin documentation][cordova-plugin-whitelist]
-
-## [www/index.html][index-html]
-
-#### Content Security Policy (CSP)
-
-The default CSP is similarly open:
-
-    <meta http-equiv="Content-Security-Policy" content="default-src * 'unsafe-inline'; style-src 'self' 'unsafe-inline'; media-src *" />
-
-Much like the access tag above, you are strongly encouraged to use a more restrictive CSP in production.
-
-A good starting point declaration might be:
-
-    <meta http-equiv="Content-Security-Policy" content="default-src 'self' data: gap: 'unsafe-inline' https://ssl.gstatic.com; style-src 'self' 'unsafe-inline'; media-src *" />
-
-For more information on the Content Security Policy, see the [section on CSP in the Cordova Whitelist Plugin documentation][cordova-plugin-whitelist-csp].
-
-Another good resource for generating a good CSP declaration is [CSP is Awesome][csp-is-awesome]
-
-
-[phonegap-cli-url]: http://github.com/phonegap/phonegap-cli
-[cordova-app]: http://github.com/apache/cordova-app-hello-world
-[bithound-img]: https://www.bithound.io/github/phonegap/phonegap-app-hello-world/badges/score.svg
-[bithound-url]: https://www.bithound.io/github/phonegap/phonegap-app-hello-world
-[config-xml]: https://github.com/phonegap/phonegap-template-hello-world/blob/master/config.xml
-[index-html]: https://github.com/phonegap/phonegap-template-hello-world/blob/master/www/index.html
-[cordova-whitelist-guide]: https://cordova.apache.org/docs/en/dev/guide/appdev/whitelist/index.html
-[cordova-plugin-whitelist]: http://cordova.apache.org/docs/en/latest/reference/cordova-plugin-whitelist
-[cordova-plugin-whitelist-csp]: http://cordova.apache.org/docs/en/latest/reference/cordova-plugin-whitelist#content-security-policy
-[csp-is-awesome]: http://cspisawesome.com
+### Run your unit tests
+```
+npm run test:unit
+```
--- src/data/Effects.js
+++ src/data/Effects.js
@@ -3,31 +3,31 @@ const randomRoll = () => Math.floor(Math.random() * 6);
 export const chemicalFactory = (diceRoll) => {
   if (diceRoll <= 4) {
     return `Euphoric for ${randomRoll()} hours`;
-  } else if (diceRoll >= 5 && diceRoll <= 8) {
+  } if (diceRoll >= 5 && diceRoll <= 8) {
     return `Hallucinogenic for ${randomRoll()} hours`;
-  } else if (diceRoll >= 9 && diceRoll <= 12) {
+  } if (diceRoll >= 9 && diceRoll <= 12) {
     return `Stimulant for ${randomRoll()} hours`;
-  } else if (diceRoll >= 13 && diceRoll <= 16) {
+  } if (diceRoll >= 13 && diceRoll <= 16) {
     return `Depressant for ${randomRoll()} hours`;
-  } else if (diceRoll >= 17 && diceRoll <= 20) {
+  } if (diceRoll >= 17 && diceRoll <= 20) {
     return 'Nutrient supplement';
-  } else if (diceRoll >= 21 && diceRoll <= 25) {
+  } if (diceRoll >= 21 && diceRoll <= 25) {
     return 'Antivenom';
-  } else if (diceRoll >= 26 && diceRoll <= 30) {
+  } if (diceRoll >= 26 && diceRoll <= 30) {
     return 'Cures disease';
-  } else if (diceRoll >= 31 && diceRoll <= 35) {
+  } if (diceRoll >= 31 && diceRoll <= 35) {
     return 'See in the dark for one hour';
-  } else if (diceRoll >= 36 && diceRoll <= 45) {
+  } if (diceRoll >= 36 && diceRoll <= 45) {
     return 'Restores a number of Might Pool points equal to cypher level';
-  } else if (diceRoll >= 46 && diceRoll <= 55) {
+  } if (diceRoll >= 46 && diceRoll <= 55) {
     return 'Restores a number of Speed Pool points equal to cypher level';
-  } else if (diceRoll >= 56 && diceRoll <= 65) {
+  } if (diceRoll >= 56 && diceRoll <= 65) {
     return 'Restores a number of Intellect Pool points equal to cypher level';
-  } else if (diceRoll >= 66 && diceRoll <= 75) {
+  } if (diceRoll >= 66 && diceRoll <= 75) {
     return 'Increases Might Edge by 1 for one hour';
-  } else if (diceRoll >= 76 && diceRoll <= 85) {
+  } if (diceRoll >= 76 && diceRoll <= 85) {
     return 'Increases Speed Edge by 1 for one hour';
-  } else if (diceRoll >= 86 && diceRoll <= 95) {
+  } if (diceRoll >= 86 && diceRoll <= 95) {
     return 'Increases Intellect Edge by 1 for one hour';
   }
   return 'Restores all Pools to full';
@@ -36,13 +36,13 @@ export const chemicalFactory = (diceRoll) => {
 export const detonation = (diceRoll) => {
   if (diceRoll <= 10) {
     return 'Cell-disrupting (harms only flesh)';
-  } else if (diceRoll >= 11 && diceRoll <= 30) {
+  } if (diceRoll >= 11 && diceRoll <= 30) {
     return 'Corrosive';
-  } else if (diceRoll >= 31 && diceRoll <= 40) {
+  } if (diceRoll >= 31 && diceRoll <= 40) {
     return 'Electrical discharge';
-  } else if (diceRoll >= 41 && diceRoll <= 50) {
+  } if (diceRoll >= 41 && diceRoll <= 50) {
     return 'Heat drain (cold)';
-  } else if (diceRoll >= 51 && diceRoll <= 75) {
+  } if (diceRoll >= 51 && diceRoll <= 75) {
     return 'Fire';
   }
   return 'Shrapnel';
@@ -51,23 +51,23 @@ export const detonation = (diceRoll) => {
 export const gasBomb = (diceRoll) => {
   if (diceRoll <= 10) {
     return 'Thick smoke: occludes sight while the cloud lasts';
-  } else if (diceRoll >= 11 && diceRoll <= 20) {
+  } if (diceRoll >= 11 && diceRoll <= 20) {
     return 'Choking gas: living creatures that breathe lose their actions to choking and coughing for a number of rounds equal to the cypher level.';
-  } else if (diceRoll >= 21 && diceRoll <= 50) {
+  } if (diceRoll >= 21 && diceRoll <= 50) {
     return 'Poison gas: living creatures that breathe suffer damage equal to the cypher level\'s';
-  } else if (diceRoll >= 51 && diceRoll <= 60) {
+  } if (diceRoll >= 51 && diceRoll <= 60) {
     return 'Corrosive gas: everything suffers damage equal to the cypher level.';
-  } else if (diceRoll >= 61 && diceRoll <= 65) {
+  } if (diceRoll >= 61 && diceRoll <= 65) {
     return 'Hallucinogenic gas: living creatures that breathe lose their actions to hallucinations and visions for a number of rounds equal to the cypher level.';
-  } else if (diceRoll >= 66 && diceRoll <= 70) {
+  } if (diceRoll >= 66 && diceRoll <= 70) {
     return 'Nerve gas: living creatures that breathe suffer Speed damage equal to the cypher level.';
-  } else if (diceRoll >= 71 && diceRoll <= 80) {
+  } if (diceRoll >= 71 && diceRoll <= 80) {
     return 'Mind-numbing gas: living creatures that breathe suffer Intellect damage equal to the cypher level.';
-  } else if (diceRoll >= 81 && diceRoll <= 83) {
+  } if (diceRoll >= 81 && diceRoll <= 83) {
     return 'Fear gas: living creatures that breathe and think flee in a random direction in fear (or are paralyzed with fear) for a number of rounds equal to the cypher level.';
-  } else if (diceRoll >= 84 && diceRoll <= 86) {
+  } if (diceRoll >= 84 && diceRoll <= 86) {
     return 'Amnesia gas: living creatures that breathe and think permanently lose all memory of the last minute.';
-  } else if (diceRoll >= 87 && diceRoll <= 96) {
+  } if (diceRoll >= 87 && diceRoll <= 96) {
     return 'Sleep gas: living creatures that breathe fall asleep for a number of rounds equal to the cypher level or until awoken by a violent action or an extremely loud noise.';
   }
   return 'Rage gas: living creatures that breathe and think make a melee attack on the nearest creature and continue to do so for a number of rounds equal to the cypher level.';
@@ -76,27 +76,27 @@ export const gasBomb = (diceRoll) => {
 export const hunterSeeker = (diceRoll) => {
   if (diceRoll <= 50) {
     return 'Inflicts 8 points of damage.';
-  } else if (diceRoll >= 51 && diceRoll <= 80) {
+  } if (diceRoll >= 51 && diceRoll <= 80) {
     return 'Bears a poisoned needle that inflicts 3 points of damage plus poison';
-  } else if (diceRoll >= 81 && diceRoll <= 90) {
+  } if (diceRoll >= 81 && diceRoll <= 90) {
     return 'Explodes, inflicting 6 points of damage to all within immediate range';
-  } else if (diceRoll >= 91 && diceRoll <= 95) {
+  } if (diceRoll >= 91 && diceRoll <= 95) {
     return 'Shocks for 4 points of electricity damage, and stuns for one round per cypher level.';
   }
-  return 'Covers target in sticky goo that immediately hardens, holding him fast until he breaks out\n' +
-    'with a Might action (difficulty equal to the cypher level + 2).';
+  return 'Covers target in sticky goo that immediately hardens, holding him fast until he breaks out\n'
+    + 'with a Might action (difficulty equal to the cypher level + 2).';
 };
 
 export const imageProjector = (diceRoll) => {
   if (diceRoll <= 20) {
     return 'Terrifying creature of an unknown species, perhaps no longer alive in the world (10-foot [3 m] cube)';
-  } else if (diceRoll >= 21 && diceRoll <= 40) {
+  } if (diceRoll >= 21 && diceRoll <= 40) {
     return 'Huge machine that obscures sight (30-foot [9 m] cube)';
-  } else if (diceRoll >= 41 && diceRoll <= 50) {
+  } if (diceRoll >= 41 && diceRoll <= 50) {
     return 'Beautiful pastoral scene (50-foot [15 m] cube)';
-  } else if (diceRoll >= 51 && diceRoll <= 60) {
+  } if (diceRoll >= 51 && diceRoll <= 60) {
     return 'Food that looks delicious but may not be familiar (10-foot [3 m] cube)\n';
-  } else if (diceRoll >= 61 && diceRoll <= 80) {
+  } if (diceRoll >= 61 && diceRoll <= 80) {
     return 'Solid color that obscures sight (50-foot [15 m] cube)';
   }
   return 'Incomprehensible scene that is disorienting and strange (20-foot [6.1 m] cube)';
@@ -105,21 +105,21 @@ export const imageProjector = (diceRoll) => {
 export const mentalScrambler = (diceRoll) => {
   if (diceRoll <= 30) {
     return 'Victims cannot act.';
-  } else if (diceRoll >= 31 && diceRoll <= 40) {
+  } if (diceRoll >= 31 && diceRoll <= 40) {
     return 'Victims cannot speak.';
-  } else if (diceRoll >= 41 && diceRoll <= 50) {
+  } if (diceRoll >= 41 && diceRoll <= 50) {
     return 'Victims move slowly (immediate range) and clumsily.';
-  } else if (diceRoll >= 51 && diceRoll <= 60) {
+  } if (diceRoll >= 51 && diceRoll <= 60) {
     return 'Victims cannot see or hear.';
-  } else if (diceRoll >= 61 && diceRoll <= 70) {
+  } if (diceRoll >= 61 && diceRoll <= 70) {
     return 'Victims lose all sense of direction, depth, and proportion.';
-  } else if (diceRoll >= 71 && diceRoll <= 80) {
+  } if (diceRoll >= 71 && diceRoll <= 80) {
     return 'Victims do not recognize anyone they know.';
-  } else if (diceRoll >= 81 && diceRoll <= 88) {
+  } if (diceRoll >= 81 && diceRoll <= 88) {
     return 'Victims suffer partial amnesia.';
-  } else if (diceRoll >= 89 && diceRoll <= 94) {
+  } if (diceRoll >= 89 && diceRoll <= 94) {
     return 'Victims suffer total amnesia.';
-  } else if (diceRoll >= 95 && diceRoll <= 98) {
+  } if (diceRoll >= 95 && diceRoll <= 98) {
     return 'Victims lose all inhibitions, revealing secrets and performing surprising actions.';
   }
   return 'Victims’ ethics are inverted.';
@@ -128,15 +128,15 @@ export const mentalScrambler = (diceRoll) => {
 export const poisonEmotion = (diceRoll) => {
   if (diceRoll <= 20) {
     return 'Anger. Likely to attack anyone who disagrees with him. Very hard to interact with; the difficulty of all such actions is increased by two steps.';
-  } else if (diceRoll >= 21 && diceRoll <= 40) {
+  } if (diceRoll >= 21 && diceRoll <= 40) {
     return 'Fear. Flees in terror for one minute when threatened.';
-  } else if (diceRoll >= 41 && diceRoll <= 60) {
+  } if (diceRoll >= 41 && diceRoll <= 60) {
     return 'Lust. Cannot focus on any nonsexual activity.';
-  } else if (diceRoll >= 61 && diceRoll <= 75) {
+  } if (diceRoll >= 61 && diceRoll <= 75) {
     return 'Sadness. The difficulty of all tasks is increased by one step.';
-  } else if (diceRoll >= 76 && diceRoll <= 85) {
+  } if (diceRoll >= 76 && diceRoll <= 85) {
     return 'Complacency. Has no motivation. The difficulty of all tasks is increased by two steps.';
-  } else if (diceRoll >= 86 && diceRoll <= 95) {
+  } if (diceRoll >= 86 && diceRoll <= 95) {
     return 'Joy. Easy to interact with in a pleasant manner; the difficulty of all such actions is decreased by one step.';
   }
   return 'Love. Much easier to interact with; the difficulty of all such actions is decreased by two steps, but temporary attachment is likely.';
@@ -145,15 +145,15 @@ export const poisonEmotion = (diceRoll) => {
 export const poisonExplosion = (diceRoll) => {
   if (diceRoll <= 25) {
     return 'The detonator is activated (must be within long range).';
-  } else if (diceRoll >= 26 && diceRoll <= 40) {
+  } if (diceRoll >= 26 && diceRoll <= 40) {
     return 'A specified amount of time passes.';
-  } else if (diceRoll >= 41 && diceRoll <= 50) {
+  } if (diceRoll >= 41 && diceRoll <= 50) {
     return 'The victim takes a specific action.';
-  } else if (diceRoll >= 51 && diceRoll <= 55) {
+  } if (diceRoll >= 51 && diceRoll <= 55) {
     return 'A specific note is sung or played on an instrument within short range.';
-  } else if (diceRoll >= 56 && diceRoll <= 60) {
+  } if (diceRoll >= 56 && diceRoll <= 60) {
     return 'The victim smells a specific scent within immediate range.';
-  } else if (diceRoll >= 61 && diceRoll <= 80) {
+  } if (diceRoll >= 61 && diceRoll <= 80) {
     return 'The victim comes within long range of the detonator.';
   }
   return 'The victim is no longer within long range of the detonator.';
@@ -162,15 +162,15 @@ export const poisonExplosion = (diceRoll) => {
 export const poisonMindControlling = (diceRoll) => {
   if (diceRoll <= 20) {
     return 'Lays down for one minute with eyes closed when told to do so.';
-  } else if (diceRoll >= 21 && diceRoll <= 40) {
+  } if (diceRoll >= 21 && diceRoll <= 40) {
     return 'Flees in terror for one minute when threatened.';
-  } else if (diceRoll >= 41 && diceRoll <= 60) {
+  } if (diceRoll >= 41 && diceRoll <= 60) {
     return 'Answers questions truthfully for one minute.';
-  } else if (diceRoll >= 61 && diceRoll <= 75) {
+  } if (diceRoll >= 61 && diceRoll <= 75) {
     return 'Attacks close friend for one round when within immediate range.';
-  } else if (diceRoll >= 76 && diceRoll <= 85) {
+  } if (diceRoll >= 76 && diceRoll <= 85) {
     return 'Obeys next verbal command given (if it is understood).';
-  } else if (diceRoll >= 86 && diceRoll <= 95) {
+  } if (diceRoll >= 86 && diceRoll <= 95) {
     return 'For 28 hours, becomes sexually attracted to the next creature of its own species that it sees.';
   }
   return 'Moves toward the next red object seen in lieu of all other actions, ignoring self preservation';
@@ -179,13 +179,13 @@ export const poisonMindControlling = (diceRoll) => {
 export const rayEmitter = (diceRoll) => {
   if (diceRoll <= 50) {
     return 'Heat/concentrated light.';
-  } else if (diceRoll >= 51 && diceRoll <= 60) {
+  } if (diceRoll >= 51 && diceRoll <= 60) {
     return 'Cell-disrupting radiation';
-  } else if (diceRoll >= 61 && diceRoll <= 80) {
+  } if (diceRoll >= 61 && diceRoll <= 80) {
     return 'Force';
-  } else if (diceRoll >= 81 && diceRoll <= 87) {
+  } if (diceRoll >= 81 && diceRoll <= 87) {
     return 'Magnetic wave';
-  } else if (diceRoll >= 88 && diceRoll <= 93) {
+  } if (diceRoll >= 88 && diceRoll <= 93) {
     return 'Molecular bond disruption.';
   }
   return 'Concentrated cold';
@@ -194,7 +194,7 @@ export const rayEmitter = (diceRoll) => {
 export const rejuvenator = (diceRoll) => {
   if (diceRoll <= 50) {
     return 'Might Pool';
-  } else if (diceRoll >= 51 && diceRoll <= 75) {
+  } if (diceRoll >= 51 && diceRoll <= 75) {
     return 'Speed Pool';
   }
   return 'Intellect Pool';
@@ -203,31 +203,30 @@ export const rejuvenator = (diceRoll) => {
 export const skillBoost = (diceRoll) => {
   if (diceRoll <= 15) {
     return 'Melee Attack';
-  } else if (diceRoll >= 16 && diceRoll <= 30) {
+  } if (diceRoll >= 16 && diceRoll <= 30) {
     return 'Ranged Attack';
-  } else if (diceRoll >= 31 && diceRoll <= 40) {
+  } if (diceRoll >= 31 && diceRoll <= 40) {
     return 'Speed Defense';
-  } else if (diceRoll >= 41 && diceRoll <= 50) {
+  } if (diceRoll >= 41 && diceRoll <= 50) {
     return 'Might Defense';
-  } else if (diceRoll >= 51 && diceRoll <= 60) {
+  } if (diceRoll >= 51 && diceRoll <= 60) {
     return 'Intellect Defense';
-  } else if (diceRoll >= 61 && diceRoll <= 68) {
+  } if (diceRoll >= 61 && diceRoll <= 68) {
     return 'Jumping';
-  } else if (diceRoll >= 69 && diceRoll <= 76) {
+  } if (diceRoll >= 69 && diceRoll <= 76) {
     return 'Climbing';
-  } else if (diceRoll >= 77 && diceRoll <= 84) {
+  } if (diceRoll >= 77 && diceRoll <= 84) {
     return 'Running';
-  } else if (diceRoll >= 85 && diceRoll <= 92) {
+  } if (diceRoll >= 85 && diceRoll <= 92) {
     return 'Swimming.';
-  } else if (diceRoll >= 93 && diceRoll <= 94) {
+  } if (diceRoll >= 93 && diceRoll <= 94) {
     return 'Sneaking';
-  } else if (diceRoll >= 95 && diceRoll <= 96) {
+  } if (diceRoll >= 95 && diceRoll <= 96) {
     return 'Balancing';
-  } else if (diceRoll >= 97 && diceRoll <= 98) {
+  } if (diceRoll >= 97 && diceRoll <= 98) {
     return 'Perceiving';
-  } else if (diceRoll >= 99 && diceRoll <= 99) {
+  } if (diceRoll >= 99 && diceRoll <= 99) {
     return 'Carrying';
   }
   return 'Escaping';
 };
-
