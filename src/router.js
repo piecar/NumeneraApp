80c7a138576347e060ddcea0009666ca95270010
Pierre Jimenez <pjime032@fiu.edu>
2019-03-15 23:38:45 -0400

    Removed extra router

--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import HelloWorld from '@/components/HelloWorld';
-import PickerViewer from '@/components/PickerViewer';
+import HelloWorld from '@/components/HelloWorld.vue';
+import PickerViewer from '@/components/PickerViewer.vue';
 
 Vue.use(Router);
 
